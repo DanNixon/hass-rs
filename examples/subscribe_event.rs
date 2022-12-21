788c5ca1c3cba265d88b70ddb6fe75cf33ee2960
Dan Nixon <dan@dan-nixon.com>
2022-12-21 19:43:20 +0000

    wip: id in return

--- examples/subscribe_event.rs
+++ examples/subscribe_event.rs
@@ -13,34 +13,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
     println!("Creating the Websocket Client and Authenticate the session");
-    let mut client = client::connect("localhost", 8123).await?;
+    let mut client = client::connect("homeassistant.castle.dan-nixon.com").await?;
 
     client.auth_with_longlivedtoken(&*TOKEN).await?;
     println!("WebSocket connection and authethication works");
 
-    //Is the callback definition usefull as it is?..or need to return anything
-    // in order to further process the result, like call for a service
     println!("Subscribe to an Event");
-
     let pet = |item: WSEvent| {
-        println!(
-        "Closure is executed when the Event with the id: {} has been received, it was fired at {}", item.id,
-        item.event.time_fired );
+        println!("doot: {:#?}", item);
     };
 
-    match client.subscribe_event("state_changed", pet).await {
-        Ok(v) => println!("Event subscribed: {}", v),
-        Err(err) => println!("Oh no, an error: {}", err),
-    }
-
-    async_std::task::sleep(std::time::Duration::from_secs(20)).await;
-
-    println!("Unsubscribe the Event");
-
-    match client.unsubscribe_event(2).await {
-        Ok(v) => println!("Succefully unsubscribed: {}", v),
-        Err(err) => println!("Oh no, an error: {}", err),
-    }
+    let id = client.subscribe_event("state_changed", pet).await.unwrap();
 
     async_std::task::sleep(std::time::Duration::from_secs(20)).await;
 
